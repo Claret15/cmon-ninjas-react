d49c99827b6bad5599d566e057deed2702ff152b
Claret15 <37543659+Claret15@users.noreply.github.com>
2019-03-15 12:19:21 +0000

    Updates

---
+++ precache-manifest.07d402a3883d62472d1cf75f5665b5f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "790dd2922b954c279e7433cf1273b04f",
+    "url": "/cmon-ninjas-react/static/media/ninja.790dd292.png"
+  },
+  {
+    "revision": "71545c1aebf35ae8345f8b19055d722e",
+    "url": "/cmon-ninjas-react/static/media/bg.71545c1a.jpg"
+  },
+  {
+    "revision": "a0595a6f1cd3a2b58be6",
+    "url": "/cmon-ninjas-react/static/js/runtime~main.a0595a6f.js"
+  },
+  {
+    "revision": "96b9d5993db2d3b0da84",
+    "url": "/cmon-ninjas-react/static/js/main.96b9d599.chunk.js"
+  },
+  {
+    "revision": "8437aee1a2ff17eae819",
+    "url": "/cmon-ninjas-react/static/js/2.8437aee1.chunk.js"
+  },
+  {
+    "revision": "96b9d5993db2d3b0da84",
+    "url": "/cmon-ninjas-react/static/css/main.8cb41fde.chunk.css"
+  },
+  {
+    "revision": "565507626bb24f442f792f2ab058ef89",
+    "url": "/cmon-ninjas-react/index.html"
+  }
+];
